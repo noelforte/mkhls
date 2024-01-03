b8b5a59d3160a11edff51d179229b35a3fe33118
Noel Forte <noel@forte.is>
2024-01-03 17:25:09 -0500

    handle mp4 fallback ouput and poster frame

--- mkhls.js
+++ mkhls.js
@@ -12,7 +12,8 @@ import { parseArgs } from 'node:util';
 
 // External modules
 import kleur from 'kleur';
-import { $ } from 'execa';
+import { $, execa, execaCommand } from 'execa';
+import * as cliProgress from 'cli-progress';
 
 // Parse command-line arguments
 const argv = parseArgs({
@@ -36,6 +37,9 @@ const argv = parseArgs({
       type: 'string',
       short: 'o',
     },
+    poster: {
+      type: 'string',
+    },
     overwrite: {
       type: 'boolean',
       default: false,
@@ -45,95 +49,121 @@ const argv = parseArgs({
 });
 
 // Extract general options into variables
-const { silent, debug } = argv.values;
+const { silent, debug, overwrite, output, poster } = argv.values;
+
+// Helper functions
+const run = (cmd) => execaCommand(cmd, { verbose: debug });
+const getTimestamp = () => {
+  const date = new Date();
+  const padField = (field, places = 0) => String(field).padStart(places, '0');
+  return `${padField(date.getHours(), 2)}:${padField(
+    date.getMinutes(),
+    2
+  )}:${padField(date.getSeconds(), 2)}.${padField(date.getMilliseconds(), 3)}`;
+};
 
 // Define logging functions
-function logger(level, message) {
+const logger = (meta, ...messages) => {
+  const [level, cmd] = meta.split(',');
+
+  if (silent || (level === 'info' && !debug)) return;
+
   const levelMap = {
     info: 'blue',
     warn: 'yellow',
     error: 'red',
   };
 
-  if (level === 'info' && !debug) return;
+  messages
+    .join(' ')
+    .split('\n')
+    .flat()
+    .forEach((message) => {
+      console.log(
+        kleur[levelMap[level]](
+          `[${getTimestamp()}] ${cmd ? `${cmd}: ${message}` : message}`
+        )
+      );
+    });
+};
 
-  console.log(kleur[levelMap[level]](`[${level}]: ${message}`));
+// Define encode options
+const encodeOptions = {
+  ffmpeg: {
+    defaultArgs: [
+      // Configure logging
+      '-loglevel error',
+      '-hide_banner',
 
-  if (level === 'error') process.exit(1);
-}
-
-// Log media info
-function showMediaFacts({ video, audio }) {
-  const fpsParams = video.r_frame_rate.split('/');
-  const fps = (fpsParams[0] / fpsParams[1]).toFixed(2);
-
-  logger(
-    'info',
-    `Selected video stream at index ${video.index}: ${video.width}x${video.height} @ ${fps}fps`
-  );
-  logger(
-    'info',
-    `Selected audio stream at index ${audio.index}: ${audio.channels}ch @ ${audio.sample_rate}Hz`
-  );
-}
+      // Set overwrite flag based on input
+      overwrite ? '-y' : '-n',
+    ],
+  },
+  hls: {
+    type: 'mpegts',
+    interval: 6,
+    segmentNames: '{resolution}p_{index}',
+    mainPlaylist: 'index.m3u8',
+    noCodecs: true,
+  },
+  video: {
+    codec: 'h264',
+    pixelFormat: 'yuv420p',
+    resolutions: [
+      { height: 2160, bitrate: '18000k', profile: 'high@5.2' },
+      { height: 1440, bitrate: '10000k', profile: 'high@5.2' },
+      { height: 1080, bitrate: '6000k', profile: 'high@5.1' },
+      { height: 720, bitrate: '3000k', profile: 'high@4.2' },
+      { height: 480, bitrate: '1500k', profile: 'high@4.0' },
+      { height: 360, bitrate: '800k', profile: 'main@3.1' },
+      { height: 240, bitrate: '600k', profile: 'main@3.1' },
+    ],
+  },
+  audio: {
+    mute: false,
+    codec: 'aac',
+    bitrate: '128k',
+  },
+  mosaic: {
+    tiles: 50,
+    tileheight: 160,
+  },
+};
 
 try {
-  // Define encode options
-  const encodeOptions = {
-    hls: {
-      type: 'mpegts',
-      interval: 6,
-      segmentNames: '{resolution}p_{index}',
-      mainPlaylist: 'index.m3u8',
-      noCodecs: true,
-    },
-    video: {
-      codec: 'h264',
-      pixelFormat: 'yuv420p',
-      resolutions: [
-        { height: 2160, bitrate: '18000k', profile: 'high@5.2' },
-        { height: 1440, bitrate: '10000k', profile: 'high@5.2' },
-        { height: 1080, bitrate: '6000k', profile: 'high@5.1' },
-        { height: 720, bitrate: '3000k', profile: 'high@4.2' },
-        { height: 480, bitrate: '1500k', profile: 'high@4.0' },
-        { height: 360, bitrate: '800k', profile: 'main@3.1' },
-        { height: 240, bitrate: '600k', profile: 'main@3.1' },
-      ],
-    },
-    audio: {
-      mute: false,
-      codec: 'aac',
-      bitrate: '128k',
-    },
-    mosaic: {
-      tiles: 50,
-      tileheight: 160,
-    },
-  };
-
   // Extract path to process
   logger('info', 'Resolving paths...');
   const sourcePath = path.resolve(argv.positionals[0]);
-  const parsedPath = path.parse(sourcePath);
+  const sourcePathData = path.parse(sourcePath);
+  const outputPath = path.resolve(
+    sourcePathData.dir,
+    output || sourcePathData.name
+  );
+
+  // Add input file to options
+  encodeOptions.ffmpeg.defaultArgs.push('-i', sourcePath);
 
   logger('info', 'Getting file data...');
+
   // Define args for ffprobe
+  const probeArgs = [
+    // Logging
+    '-hide_banner -loglevel fatal',
+
+    // Make sure the returned data shows select items
+    '-show_error',
+    '-show_format',
+    '-show_streams',
+    '-show_programs',
+    '-show_chapters',
+    '-show_private_data',
+
+    // Format output as JSON
+    '-print_format json',
+  ];
+
   const fileData = JSON.parse(
-    (
-      await $`ffprobe ${[
-        '-hide_banner',
-        '-loglevel',
-        'fatal',
-        '-show_error',
-        '-show_format',
-        '-show_streams',
-        '-show_programs',
-        '-show_chapters',
-        '-show_private_data',
-        '-print_format',
-        'json',
-      ]} ${sourcePath}`
-    ).stdout
+    (await run(`ffprobe ${probeArgs.join(' ')} ${sourcePath}`)).stdout
   );
 
   // Select streams
@@ -142,26 +172,99 @@ try {
     audio: fileData.streams.find((stream) => stream.codec_type === 'audio'),
   };
 
-  showMediaFacts(streams);
+  if (streams.video) {
+    const fpsParams = streams.video.r_frame_rate.split('/');
+    const fps = (fpsParams[0] / fpsParams[1]).toFixed(2);
+
+    logger(
+      'info,ffprobe',
+      `Selected video stream at index ${streams.video.index}:`,
+      `${streams.video.width}x${streams.video.height}`,
+      `@ ${fps}fps`
+    );
+  } else {
+    logger('warn', `no video tracks available in ${path}`);
+  }
+
+  if (streams.audio) {
+    logger(
+      'info,ffprobe',
+      `Selected audio stream at index ${streams.audio.index}:`,
+      `${streams.audio.channels}ch`,
+      `@ ${streams.audio.sample_rate}Hz`
+    );
+  } else {
+    logger('warn', `no audio tracks available in ${path}`);
+  }
 
   // Filter sizes
   const filteredResolutions = encodeOptions.video.resolutions.filter(
     (resolution) => {
       if (resolution.height > streams.video.height) {
-        logger('info', `skipping ${resolution.height}p resolution`);
+        logger(
+          'warn',
+          `skipping ${resolution.height}p resolution as video resolution is ${streams.video.height}p`
+        );
       }
 
       return resolution.height <= streams.video.height;
     }
   );
 
+  logger('info', 'create output directory');
+  await fs.mkdir(path.resolve(sourcePathData.dir, sourcePathData.name), {
+    recursive: true,
+  });
+
+  // Handle poster frame creation
+  if (streams.video && poster) {
+    const posterPaths = [
+      path.resolve(poster),
+      path.resolve(outputPath, `poster${path.extname(poster)}`),
+    ];
+    logger(
+      'info',
+      `copying poster frame from ${posterPaths[0]} to ${posterPaths[1]}`
+    );
+    await fs.cp(posterPaths[0], posterPaths[1]);
+  } else if (streams.video) {
+    logger('info', `generating poster frame from input`);
+    const posterArgs = [
+      // Insert defaults
+      ...encodeOptions.ffmpeg.defaultArgs,
+
+      // Seek to 5%
+      `-ss ${Math.round(Number(streams.video.duration) * 0.05)}`,
+
+      // Select video track
+      `-map 0:${streams.video.index}`,
+
+      // Use 1 frame
+      '-frames:v 1',
+
+      // Filter to select I frame and scale
+      `-vf select=eq(pict_type\\,I),scale=-1:${streams.video.height}`,
+
+      // Use a JPEG quality of ~ 10
+      '-qscale:v 28',
+
+      // Set output
+      `${outputPath}/poster.jpg`,
+    ];
+
+    // Await execution
+    await run(`ffmpeg ${posterArgs.join(' ')}`);
+  }
+
+  logger('info', 'building ffmpeg argument list...');
+
   // Initialize ffmpeg argument list
   const transcodeArgs = [
-    // Minimal logging
-    '-loglevel error -hide_banner',
+    // Insert defaults
+    ...encodeOptions.ffmpeg.defaultArgs,
 
-    // Use sourcefile path
-    `-i ${sourcePath}`,
+    // Show progress
+    '-progress -',
   ];
 
   // If we're NOT skipping a fallback, add those options
@@ -169,33 +272,82 @@ try {
     transcodeArgs.push(
       // Use mp4 as the format
       '-f mp4',
-      // Selec video track
+
+      // Select video track
       `-map 0:${streams.video.index}`,
+
       // Scale to 720p
       `-vf scale=-1:720,format=${encodeOptions.video.pixelFormat}`,
+
       // Use h264 as video codec
       `-c:v ${encodeOptions.video.codec}`,
+
       // Set video profile
-      '-profile:v main -level:v 3.1',
+      '-profile:v main',
+      '-level:v 3.1',
+
       // Set bitrate options
-      '-b:v 2000k -maxrate:v 2000k -bufsize:v 3000.0k',
+      '-b:v 2000k',
+      '-maxrate:v 2000k',
+      '-bufsize:v 3000.0k',
+
       // Select audio track
       `-map 0:${streams.audio.index}`,
+
       // Select audio codec
       `-c:a ${encodeOptions.audio.codec}`,
+
       // Reuse same sample rate from input
       `-ar ${streams.audio.sample_rate}`,
+
       // Set audio profile
       '-profile:a aac_low',
+
       // Set audio bitrate
       '-b:a 96k',
+
       // Move index to beginning
       '-movflags +faststart',
+
       // Set output filename
-      'progressive.mp4'
+      `${outputPath}/progressive.mp4`
     );
 
-  console.log(transcodeArgs.join(' '));
+  const transcode = run(`ffmpeg ${transcodeArgs.join(' ')}`);
+
+  const progress = new cliProgress.MultiBar(
+    {
+      format: '{title} [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}',
+      clearOnComplete: true,
+    },
+    cliProgress.Presets.legacy
+  );
+
+  const mp4progress = progress.create();
+
+  mp4progress.start(Number(streams.video.nb_frames), 0, {
+    title: 'MP4 Fallback',
+  });
+
+  transcode.stdout.on('data', (data) => {
+    const output = data.toString();
+    const stats = output.split('\n').reduce((statsObject, currentElement) => {
+      const prop = currentElement.split('=');
+      statsObject[prop[0]] = prop[1];
+      return statsObject;
+    }, {});
+
+    mp4progress.update(Number(stats.frame));
+  });
+
+  transcode.stderr.on('data', (data) => {
+    const output = data.toString();
+    logger('error,ffmpeg', output);
+  });
+
+  transcode.stdout.on('close', () => {
+    progress.stop();
+  });
 } catch (error) {
   logger('error', error.message);
 }
