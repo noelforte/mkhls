ef61d166bb322506604b36e0aa0a8a89a472aad8
Noel Forte <noel@forte.is>
2024-01-12 23:14:53 -0500

    refactor: split options out to seperate file

--- mkhls.js
+++ mkhls.js
@@ -13,61 +13,13 @@ import crypto from 'node:crypto';
 // Helper modules
 import logger from './utils/logger.js';
 import convertTime from './utils/convertTime.js';
-import getArgs from './lib/getArgs.js';
 import FFmpeg from './lib/ffmpeg.js';
+import opts from './lib/getOpts.js';
 
 // External modules
 import sharp from 'sharp';
 import { globSync } from 'glob';
-
-const opts = {
-	positionals: getArgs.positionals,
-	...getArgs.values,
-	hls: {
-		type: 'mpegts',
-		interval: 6,
-		segmentName: '{stream}/segment_{index}',
-		mainPlaylist: 'manifest.m3u8',
-		noCodecs: true,
-	},
-	video: {
-		codec: 'libx264',
-		pixelFormat: 'yuv420p',
-		resolutions: [
-			{ height: 2160, bitrate: '18000k', profile: 'high@5.2' },
-			{ height: 1440, bitrate: '10000k', profile: 'high@5.2' },
-			{ height: 1080, bitrate: '6000k', profile: 'high@5.1' },
-			{ height: 720, bitrate: '3000k', profile: 'high@4.2' },
-			{ height: 480, bitrate: '1500k', profile: 'high@4.0' },
-			{ height: 360, bitrate: '800k', profile: 'main@3.1' },
-			{ height: 240, bitrate: '600k', profile: 'main@3.1' },
-		],
-	},
-	audio: {
-		mute: false,
-		codec: 'aac',
-		profile: 'aac_low',
-		bitrate: '256k',
-	},
-	mosaic: {
-		imagesPerRow: 6,
-		tileheight: 144,
-		minInterval: 1,
-		maxInterval: 10,
-		maxImages: 180,
-	},
-};
-
-// Instance ffmpeg handler
-const transcode = new FFmpeg();
-
-// Set overwrite option
-transcode.addArguments(opts.overwrite ? '-y' : '-n');
-
-let tmpPath;
-
-try {
-	if (opts.positionals.length !== 1) {
+import kleur from 'kleur';
 		throw new Error(
 			`Need 1 non-option argument, recieved ${opts.positionals.length}`
 		);
