253e7e84876d73e06532f0145b9bf9e8fbcfe022
Noel Forte <noel@forte.is>
2024-01-14 01:28:32 -0500

    add docs to logger

--- utils/logger.js
+++ utils/logger.js
@@ -3,7 +3,18 @@ import getArgs from '../lib/getArgs.js';
 import getTimestamp from './getTimestamp.js';
 const { values: opts } = getArgs;
 
-// Logging function
+/**
+ * Simple logging utility that wraps `console.log` calls with {@link https://www.npmjs.com/package/kleur kleur}
+ *
+ * @param {LogFormat} meta
+ *
+ * @param {string[]} messages
+ * An array of messages to pass to the logging utility. Each message will be processed as a seperate line.
+ *
+ * @typedef {'<level>,<program>'} LogFormat
+ * A comma-seperated string specifying `<level>` and `<program>`, where
+ * `<level>` is one of `info`, `event`, `warn`, or `error`.
+ */
 export default (meta, ...messages) => {
 	// Set up timestamp object and level
 	const [level, cmd] = meta.split(',');
